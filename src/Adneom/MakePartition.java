0edcc4e87bc32807f4a5adb6319a491993ef56b2
Sofienne21 <64814889+Sofienne21@users.noreply.github.com>
2020-05-06 14:57:37 +0200

    first commit

--- src/Adneom/MakePartition.java
+++ src/Adneom/MakePartition.java
@@ -15,7 +15,7 @@ public class MakePartition {
 	
 	public static<T> List[] partition(List<T> myList, int n){
 		
-		//Vérifier les parametres
+		//VÃ©rifier les parametres
 		checkParams(myList, n);
 		
 		//Prendre la taille de la liste
@@ -27,7 +27,7 @@ public class MakePartition {
 			nbrPart++;
 		}
 		
-		//Creation d'une nouvelle liste à partir de la liste initial
+		//Creation d'une nouvelle liste Ã  partir de la liste initial
 		List<T>[] toReturn = new ArrayList[nbrPart];
 		
 		for (int i = 0; i < nbrPart; i++)
@@ -41,26 +41,4 @@ public class MakePartition {
 		return toReturn;
 		
 	}
-
-	public static void main(String[] args) {
-		List<Integer> myList = new ArrayList<>();
-		myList.add(1);
-		myList.add(2);
-		myList.add(3);
-		myList.add(4);
-		myList.add(5);
-		
-		List[] finalList = partition(myList, 2);
-		for(int i=0; i<finalList.length; i++){
-			System.out.print("[");
-			for(int j=0; j<finalList[i].size(); j++){
-				System.out.print(finalList[i].get(j));
-				if(j<finalList[i].size()-1){
-					System.out.print(",");
-				}
-			}
-			System.out.print("]");
-		}
-	}
-
 }
